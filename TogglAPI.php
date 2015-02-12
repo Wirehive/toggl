8ac2c2e03c1ddaa7142d54609342fdf40338edce
Robin Corps <robin@ngse.co.uk>
2015-02-12 13:18:12 +0000

    Adding stub

---
+++ TogglAPI.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * Class TogglAPI
+ *
+ * Interface to the Toggl API v8
+ *
+ * @author Robin Corps <robin@wirehive.net>
+ * @version 0.1
+ * @package toggl
+ */
+class TogglAPI
+{
+
+}
